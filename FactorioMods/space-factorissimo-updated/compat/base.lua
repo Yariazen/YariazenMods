13c481c96404562c59c362e05c3caaf126668932
Yariazen <35210313+Yariazen@users.noreply.github.com>
2023-01-15 15:18:58 -0800

    Whoops did that wrong

--- FactorioMods/space-factorissimo-updated/compat/base.lua
+++
@@ -1,5 +0,0 @@
-require("scripts.utility")
-local interior_tile = collision_mask_util_extended.get_make_named_collision_mask("interior-tile")
-
--- Artillery Turret
-add_collision_layer("artillery-turret", "artillery-turret", interior_tile)
--- FactorioMods/space-factorissimo-updated/prototypes/input.lua
+++
@@ -1,9 +0,0 @@
-function input_prototype(name, key_sequence)
-	data:extend ({
-		{
-			type = "custom-input",
-			name = name,
-			key_sequence = key_sequence
-		}
-	})
-end
